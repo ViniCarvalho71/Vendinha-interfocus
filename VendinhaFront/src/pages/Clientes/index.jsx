0e901d534f3d768a3aeac6fc8cbf3f62549e1386
ViniCarvalho71 <carvalhovinicius71@gmail.com>
2025-07-06 23:50:04 -0300

    Listando, excluindo e salvando clientes no front

--- VendinhaFront/src/pages/Clientes/index.jsx
+++ VendinhaFront/src/pages/Clientes/index.jsx
@@ -1,6 +1,107 @@
-export function TabelaCliente(){
+import { FaPlus,FaTrashAlt, FaEye, FaEdit } from "react-icons/fa";
+import { listarClientes, salvarCliente, excluirCliente} from "../../services/clienteService";
+import Modal from "../../components/Modal";
+import { useState, useEffect} from "react";
+
+
+
+export function LinhaCliente({cliente, excluir, edit}){
+    const data = new Date(cliente.dataNascimento);
+    return(
+        <tr>
+                    <td>{cliente.nome}</td>
+                    <td>{cliente.cpf}</td>
+                    <td>{data.toLocaleString()}</td>
+                    <td>{cliente.dividas.length === 0 ? "Não Deve" : "Deve"}</td>
+                    <td className="actions">
+                        <button className="visualizar" id={cliente.id}><FaEye className="action-icon" /></button>
+                        <button className="editar" id={cliente.id} onClick={edit}><FaEdit className="action-icon" /></button>
+                        <button className="excluir" id={cliente.id} onClick={excluir}
+                        ><FaTrashAlt className="action-icon" /></button>
+                    </td>
+                </tr>
+    );
+}
+
+export default function ClientePage(){
+
+    const [clientes, setClientes] = useState([]);
+    const [search, setSearch] = useState();
+    const [page, setPage] = useState(1);
+    const [selected, setSelected] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [openDeletar, setOpenDeletar] = useState(false);
+
+    const fetchData = async () => {
+        const resultado = await listarClientes(search, page);
+        if (resultado.status == 200) {
+            setClientes(resultado.data);
+        }
+    }
+
+    const submitForm = async (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const formData = new FormData(form);
+
+        const cliente = {
+            nome: formData.get("nome"),
+            cpf: formData.get("cpf"),
+            email: formData.get("email"),
+            dataNascimento: formData.get("data_nascimento"),
+            descricao: formData.get("descricao")
+        };
+        // falsey values: 0, null, undefined, "", false
+        // not falsey values = TRUE
+        if (selected?.id) {
+            cliente.id = selected.id;
+        }
+        const resultado = await salvarCliente(cliente);
+        if (resultado.status == 200) {
+            setOpen(false);
+            fetchData();
+        }
+    }
+    useEffect(() => {
+        var timeout = setTimeout(() => {
+            fetchData();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timeout);
+        }
+    }, [search, openDeletar]);
+
+    const abrirModalDelete = (cliente) => {
+        setSelected(cliente);
+        setOpenDeletar(true);
+    }
+
+    const abrirModalEdit = (cliente) => {
+        setSelected(cliente);
+        setOpen(true);
+    }
+
+    const excluirClientePorId = async () => {
+        const resultado = await excluirCliente(selected?.id);
+        if (resultado.status == 200) {
+            setSelected(null);
+            setOpenDeletar(false);
+        }
+    }
+
 
     return (
+        <>
+        <div className="tabela-container">
+        <div className="tabela-header">
+        <input type="text" placeholder="Pesquisa"
+                onChange={(e) =>
+                    setSearch(e.target.value)
+                }/>
+        <button onClick={() => {setOpen(true); setSelected(null);}}><FaPlus/></button>
+        </div>
         <table>
             <thead>
                 <tr>
@@ -8,44 +109,56 @@ export function TabelaCliente(){
                     <th>Cpf</th>
                     <th>Data Nascimento</th>
                     <th>Situação</th>
+                    <th>Ações</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>Vinícius</td>
-                    <td>158.058.898-38</td>
-                    <td>00/00/00</td>
-                    <td>Deve</td>
-                </tr>
-                <tr>
-                    <td>Vinícius Carvalho da Silva</td>
-                    <td>158.058.898-38</td>
-                    <td>00/00/00</td>
-                    <td>Deve</td>
-                </tr>
-                <tr>
-                    <td>Vinícius Carvalho da Silva</td>
-                    <td>158.058.898-38</td>
-                    <td>00/00/00</td>
-                    <td>Deve</td>
-                </tr>
-                <tr>
-                    <td>Vinícius Carvalho da Silva</td>
-                    <td>158.058.898-38</td>
-                    <td>00/00/00</td>
-                    <td>Deve</td>
-                </tr>
+                {
+                    clientes.map(cliente =>
+                        <LinhaCliente key={cliente.id}
+                            cliente={cliente}
+                            excluir={() => abrirModalDelete(cliente) }
+                            edit = {()=> abrirModalEdit(cliente)}
+                            ></LinhaCliente>
+                    )
+                }
             </tbody>
         </table>
-    );
-}
+        <div className="tabela-footer">
+            <div className="paginacao">
+                <button>Anter.</button>
+                <p>1 de 3</p>
+                <button>Prox.</button>
+            </div>
+        </div>
+        </div>
 
-export default function ClientePage(){
-
-    return (
-        <>
-        <h1>Tabela de Clientes</h1>
-        <TabelaCliente></TabelaCliente>
+        <Modal open={open}>
+            <form method="post" onSubmit={submitForm}>
+                <input defaultValue={selected?.nome} id="cliente-nome" required minLength="10" maxLength="50" name="nome" type="text" placeholder="Nome" />
+                <input type="text" defaultValue={selected?.cpf} name="cpf" placeholder="Cpf"/>
+                <input type="email" defaultValue={selected?.email} name="email" placeholder="Email"/>
+                <label>Data de Nascimento:</label>
+                <input type="date" defaultValue={selected?.dataNascimento} name="data_nascimento" />
+                <textarea defaultValue={selected?.descricao} required name="descricao" placeholder="Descrição"></textarea>
+                <div className="row">
+                    <button type="reset" onClick={() => setOpen(false)}>Cancelar</button>
+                    <button type="submit">Cadastrar</button>
+                </div>
+            </form>
+        </Modal>
+                <Modal open={openDeletar}>
+            <div className="modal-deletar">
+                <p>Deseja excluir o cliente {selected?.nome}?</p>
+                <div className="row">
+                    <div className="row">
+                            <button type="reset" onClick={() => setOpenDeletar(false)}>Cancelar</button>
+                            <button type="submit" onClick={() => excluirClientePorId()}>Excluir</button>
+                        </div>
+                </div>
+            </div>
+        </Modal>
+        
         </>
     );
 }
---
+++ VendinhaFront/src/services/clienteService.js
@@ -0,0 +1,60 @@
+const URL_API = "http://localhost:5230";
+
+export async function salvarCliente(cliente) {
+    const resultado = await fetch(`${URL_API}/api/cliente`, {
+        method: cliente.id ? "PUT" : "POST",
+        body: JSON.stringify(cliente),
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+    var dados = await resultado.json();
+    return {
+        status: resultado.status,
+        data: dados
+    }
+}
+
+export function listarClientes(busca,page) {
+    return fetch(`${URL_API}/api/cliente?pesquisa=${busca || ""}&page=${page}`, {
+        method: "GET"
+    }).then(async resultado => {
+        if (resultado.status === 200) {
+            const data = await resultado.json();
+            return {
+                status: resultado.status,
+                data: data
+            }
+        }
+        return {
+            status: resultado.status,
+            data: null
+        }
+    })
+
+}
+
+export async function excluirCliente(id) {
+    const resultado = await fetch(`${URL_API}/api/cliente/${id}`, {
+        method: "DELETE" ,
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+    var dados = await resultado.json();
+    return {
+        status: resultado.status,
+        data: dados
+    }
+}
+
+export async function getAlunoById(id) {
+    const resultado = await fetch(`${URL_API}/api/cliente/${id}`, {
+        method: "GET"
+    });
+    var dados = await resultado.json();
+    return {
+        status: resultado.status,
+        data: dados
+    }
+}
